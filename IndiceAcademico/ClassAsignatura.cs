6007dc0ca7a60c6e8189f6fc9451249cfdd69017
Leand3k <theleand3000@gmail.com>
2019-06-28 18:04:22 -0400

    .

---
+++ IndiceAcademico/ClassAsignatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndiceAcademico
+{
+    class ClassAsignatura
+    {
+        public string Clave { get; set; }
+
+        public string NombreAsig { get; set; }
+
+        public int Credito { get; set; }
+
+        
+
+        
+    }
+}
---
+++ IndiceAcademico/ClassEstudiante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndiceAcademico
+{
+    class ClassEstudiante
+    {
+        public int ID { get; set; }
+
+        public string NombreEst { get; set; }
+
+        public string Carrera { get; set; }
+
+    }
+}
