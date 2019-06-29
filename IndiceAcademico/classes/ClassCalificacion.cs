1146ef6edc1010cf67f86698ea35fd8e9ab18c9b
KELVIN LEE HSU <1087781@est.intec.edu.do>
2019-06-28 23:25:46 -0400

    Agregar ClassCalificacion

---
+++ IndiceAcademico/classes/ClassCalificacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndiceAcademico
+{
+    public class Asignatura
+    {
+        public int Nota { get; set; }
+
+        public Asignatura asignatura { get; set; }
+
+        /*
+         Si public Asignatura asignatura no sirve, pues use:
+         public string Asignatura {get; set;}
+         */
+
+        
+
+        
+    }
+}
