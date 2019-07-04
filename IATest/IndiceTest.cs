0d868b2a71fa4ee6f907dcbc71eccfa35ff3ad19
FRANK ROBERTO CHAVEZ SOSA <robertofrank2000@hotmail.com>
2019-07-04 15:48:22 -0400

    Arreglado Test

---
+++ IATest/IndiceTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IndiceAcademico;
+
+namespace IATest
+{
+    [TestClass]
+    public class IndiceTest
+    {
+        [TestClass]
+        public class ManejoNotas
+        {
+            [TestMethod]
+            public void ValorNota_Test()
+            {
+                IndiceCalc index = new IndiceCalc();
+                Calificacion cal = new Calificacion();
+                cal.Nota = 90.00;
+                double expected = 4;
+                double value = index.ValorNota(cal);
+
+                Assert.AreEqual(value, expected);
+
+            }
+
+            [TestMethod]
+            public void LetraNota_Test()
+            {
+                IndiceCalc index = new IndiceCalc();
+                Calificacion cal = new Calificacion();
+                cal.Nota = 80.00;
+                string letraNota = index.LetraNota(cal);
+                string expected = "B";
+
+                Assert.AreEqual(letraNota, expected);
+
+            }
+
+        }
+        [TestClass]
+        public class Calculos
+        {
+            [TestMethod]
+            public void CalcularPuntosHonor_Test()
+            {
+                IndiceCalc index = new IndiceCalc();
+                Calificacion cal = new Calificacion();
+                Asignatura asignatura = new Asignatura { Creditos = 4 };
+
+                cal.Asignatura = asignatura;
+                cal.Nota = 86.00;
+
+                double value = index.CalcularPuntosHonor(cal);
+                double expected = 14;
+
+                Assert.AreEqual(value, expected);
+
+            }
+
+            [TestMethod]
+            public void ObtenerCreditos_Test()
+            {
+                IndiceCalc index = new IndiceCalc();
+                Calificacion cal1 = new Calificacion();
+                Asignatura asignatura = new Asignatura() { Creditos = 4 };
+
+                cal1.Asignatura = asignatura;
+
+
+                double expected = 4;
+                double value = index.ObtenerCreditos(cal1);
+
+
+                Assert.AreEqual(value, expected);
+
+            }
+
+
+        }
+    }
+}
---
+++ IATest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IATest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IATest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03d946a2-cd47-45eb-9047-0ad0695cac64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
